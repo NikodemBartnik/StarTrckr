rotation matrix fix, clean up

experiments/PlotVisualizer.py
@@ -15,7 +15,7 @@ class PlotVisualizer:
         self.tracker_vec_x = tracker_vec_x
         self.tracker_vec_z = tracker_vec_z
 
-        self.ani = animation.FuncAnimation(self.fig, self.updatePlot, frames=np.arange(0, 360, 1), interval=1)
+        self.ani = animation.FuncAnimation(self.fig, self.updatePlot, frames=np.arange(0, 360, 1), interval=50)
         plt.ion()
         plt.show()
 
@@ -29,7 +29,6 @@ class PlotVisualizer:
 
 
     def updatePlot(self, a):
-        print('update called')
         self.ax.set_xlabel(r'$x$', fontsize='large')
         self.ax.set_ylabel(r'$y$', fontsize='large')
         self.ax.set_zlabel(r'$z$', fontsize='large')

experiments/TrackerController.py
@@ -1,7 +1,5 @@
 import TrackerMath as tm
 import numpy as np
-import math
-
 
 class TrackerController:
     def __init__(self):
@@ -18,7 +16,7 @@ class TrackerController:
 
     def rotate(self, x, y, z):
         self.tracker_vec_x = tm.rotateNormal(self.tracker_vec_x, x, y, z)
-        #self.tracker_vec_z = tm.rotateNormal(self.tracker_vec_z, x, y, z)
+        self.tracker_vec_z = tm.rotateNormal(self.tracker_vec_z, x, y, z)
 
     def rotateZ(self, x):
         self.tracker_vec_z = tm.rotateNormal(self.tracker_vec_z, x, 0, 0)
@@ -29,18 +27,12 @@ class TrackerController:
 
 
     def getA(self):
-        #return np.round(np.arccos(np.dot(self.tracker_vec_x, self.ref_vec_z)) * 180/np.pi, 2) - 90
-        #return np.arccos(np.clip(np.dot(self.tracker_vec_x, self.ref_vec_z), -1.0, 1.0)) * 180/np.pi - 90
         return np.arctan2(self.tracker_vec_x[1], self.tracker_vec_x[0]) * 180/np.pi
 
     def getB(self):
-        #return np.round(np.arccos(np.dot(self.tracker_vec_x, self.ref_vec_y)) * 180/np.pi, 2) - 90
-        #return np.arccos(np.clip(np.dot(self.tracker_vec_x, self.ref_vec_y), -1.0, 1.0)) * 180/np.pi - 90
         return np.arctan2(self.tracker_vec_z[1], self.tracker_vec_z[2]) * 180/np.pi
 
     def getC(self):
-        #return np.round(np.arccos(np.dot(self.tracker_vec_z, self.ref_vec_x)) * 180/np.pi, 2) - 90
-        #return np.arccos(np.clip(np.dot(self.tracker_vec_z, self.ref_vec_x), -1.0, 1.0)) * 180/np.pi - 90
         return np.arctan2(self.tracker_vec_x[0], self.tracker_vec_x[2]) * 180/np.pi - 90
 
     def getRefVecX(self):

experiments/TrackerMath.py
@@ -1,15 +1,13 @@
 import numpy as np
-import math
 
 def rotateNormal(vector, angle_x, angle_y, angle_z):
     a = np.pi/180 * angle_x
     b = np.pi/180 * angle_y
     c = np.pi/180 * angle_z
-    rotation_matrix = np.array([[np.cos(b)*np.cos(c), np.sin(a)*np.sin(b)*np.cos(c), np.cos(a)*np.sin(b)*np.cos(c)+np.sin(a)*np.sin(c)],
+    rotation_matrix = np.array([[np.cos(b)*np.cos(c), np.sin(a)*np.sin(b)*np.cos(c)-np.cos(a)*np.sin(c), np.cos(a)*np.sin(b)*np.cos(c)+np.sin(a)*np.sin(c)],
                             [np.cos(b)*np.sin(c), np.sin(a)*np.sin(b)*np.sin(c)+np.cos(a)*np.cos(c), np.cos(a)*np.sin(b)*np.sin(c)-np.sin(a)*np.cos(c)],
                             [-np.sin(b), np.sin(a)*np.cos(b), np.cos(a)*np.cos(b)]])
-    ret_vec = np.matmul(rotation_matrix, vector)
-    return ret_vec/np.linalg.norm(ret_vec)
+    return np.dot(rotation_matrix, vector)
 
 def rotateAroundAxis(vector, axis, angle):
     #vector: [ux, uy, uz]
@@ -17,5 +15,4 @@ def rotateAroundAxis(vector, axis, angle):
     rotation_matrix = np.array([[np.cos(t) +pow(axis[2], 2)*(1-np.cos(t)), axis[0]*axis[1]*(1-np.cos(t)) - axis[2]*np.sin(t), axis[0]*axis[2]*(1-np.cos(t)) + axis[1]*np.sin(t)],
                                 [axis[1]*axis[0]*(1-np.cos(t)) + axis[2]*np.sin(t), np.cos(t)+pow(axis[1], 2)*(1-np.cos(t)), axis[1]*axis[2]*(1-np.cos(t))-axis[0]*np.sin(t)],
                                 [axis[2]*axis[0]*(1-np.cos(t))-axis[1]*np.sin(t), axis[2]*axis[1]*(1-np.cos(t))+axis[0]*np.sin(t), np.cos(t)+pow(axis[2], 2)*(1-np.cos(t))]])
-    ret_vec = np.matmul(rotation_matrix, vector)
-    return ret_vec/np.linalg.norm(ret_vec)
+    return np.matmul(rotation_matrix, vector)

experiments/controller.py
@@ -1,7 +1,5 @@
-import pygame, math, time, gui
+import pygame, time, gui
 import serial
-import numpy as np
-import TrackerMath as tm
 import TrackerController
 import PlotVisualizer
 
@@ -80,9 +78,9 @@ while 1:
         tc.rotate(0, 0.3, 0)
 
     if a[0] > 140 and a[0] < 210 and a[1] > 0.5:
-        tc.rotate(0, 0, -0.3)
+        tc.rotate(0, 0, -1)
     if a[0] > 320 or a[0] < 40 and a[1] > 0.5:
-       tc.rotate(0, 0, 0.3)
+       tc.rotate(0, 0, 1)
 
     if button_turn_right.getValue():
         tc.rotateZ(0.3)
@@ -91,25 +89,19 @@ while 1:
 
     if button_set_axis.getValue():
         tc.polarAlign()
-        #plotVisualizer = PlotVisualizer.PlotVisualizer(tc.getRefVecX(), tc.getRefVecZ(), tc.getPolarVecX(), tc.getPolarVecZ(), tc.getTrackerVecX(), tc.getTrackerVecZ())
+        plotVisualizer = PlotVisualizer.PlotVisualizer(tc.getRefVecX(), tc.getRefVecZ(), tc.getPolarVecX(), tc.getPolarVecZ(), tc.getTrackerVecX(), tc.getTrackerVecZ())
         setAxis = True
 
     if button_track.getValue():
         if last_tracking_update == 0:
             pass
         else:
-            #new_pos = rotateAroundAxis(np.array([angle_a, angle_b, angle_c]), rot_axis, -((time.time() - last_tracking_update)/86164 * 360))
-            #new_pos = rotateAroundAxis(np.array([angle_a, angle_b, angle_c]), rot_axis, -((time.time() - last_tracking_update)/60 * 360))
-            
-            #angle_a = new_pos[0]
-            #angle_b = new_pos[1]
-            #angle_c = new_pos[2]
             tc.track(-((time.time() - last_tracking_update)/120 * 360))
         last_tracking_update = time.time()
 
     
-    #if(setAxis):
-        #plotVisualizer.updateVectors(tc.getRefVecX(), tc.getRefVecZ(), tc.getPolarVecX(), tc.getPolarVecZ(), tc.getTrackerVecX(), tc.getTrackerVecZ())
+    if(setAxis):
+        plotVisualizer.updateVectors(tc.getRefVecX(), tc.getRefVecZ(), tc.getPolarVecX(), tc.getPolarVecZ(), tc.getTrackerVecX(), tc.getTrackerVecZ())
         
 
     line = 'A' + str(tc.getA()) + ' B' + str(tc.getC()) +' C' + str(tc.getB()) + '\n'