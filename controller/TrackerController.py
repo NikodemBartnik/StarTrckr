308ec6bc94c114b6ca44c7c002f202caf18d4829
NikodemBartnik <nikodem.bartnik@gmail.com>
2024-05-21 08:36:06 +0200

    test

--- controller/TrackerController.py
+++ controller/TrackerController.py
@@ -18,13 +18,11 @@ class TrackerController:
         self.tracker_vec_x = self.origin_vec_x
         self.tracker_vec_y = self.origin_vec_y
         self.tracker_vec_z = self.origin_vec_z
-        
 
     def polarAlign(self):
         self.polar_vec_x = self.tracker_vec_x
         self.polar_vec_y = self.tracker_vec_x
         self.polar_vec_z = self.tracker_vec_z
-
 
     def rotateAltitude(self, a):
         self.tracker_vec_x = tm.rotateAroundAxis(self.tracker_vec_x, self.tracker_vec_y, a)
@@ -42,79 +40,36 @@ class TrackerController:
         self.tracker_vec_y = tm.rotateAroundAxis(self.tracker_vec_y, self.tracker_vec_x, x)
         self.tracker_vec_z = tm.rotateAroundAxis(self.tracker_vec_z, self.tracker_vec_x, x)
 
-
     def track(self, angle):
         self.tracker_vec_x = tm.rotateAroundAxis(self.tracker_vec_x, self.polar_vec_x, angle)
         self.tracker_vec_y = tm.rotateAroundAxis(self.tracker_vec_y, self.polar_vec_x, angle)
         self.tracker_vec_z = tm.rotateAroundAxis(self.tracker_vec_z, self.polar_vec_x, angle)
 
+    def calculateRA(self):
 
-    def getA(self):
-        return np.sign(self.tracker_vec_x[1])*np.arccos(self.tracker_vec_x[0]/(np.sqrt(pow(self.tracker_vec_x[0], 2) + pow(self.tracker_vec_x[1], 2)))) * 180/np.pi
+        ra = np.arctan2(self.ref_vec_y[1], self.ref_vec_x[0])
+        ra = np.degrees(ra) / 15.0
+        if ra < 0:
+            ra += 24
+        return ra
 
+    def calculateDEC(self):
+        dec = np.arcsin(self.ref_vec_z[2])
+        dec = np.degrees(dec)
+        return dec
 
-    def getB(self):
-        return np.arccos(self.tracker_vec_x[2]) * 180/np.pi - 90
-
-
-    def getC(self):
-        angle = np.degrees(np.arctan2(np.linalg.norm(np.cross(self.ref_vec_z, self.tracker_vec_z)), np.dot(self.ref_vec_z, self.tracker_vec_z)))
-        if((np.cross(self.ref_vec_z, self.tracker_vec_z)[0] < 0 or self.tracker_vec_x[0] < 0) and not (np.cross(self.ref_vec_z, self.tracker_vec_z)[0] <= 0 and self.tracker_vec_x[0] <= 0)):
-            angle = -angle
-        return angle
-
-
-
-    def getRefVecX(self):
-        return self.origin_vec_x
-
-    def getRefVecZ(self):
-        return self.origin_vec_z
-
-    def getPolarVecX(self):
-        return self.polar_vec_x
-
-    def getPolarVecZ(self):
-        return self.polar_vec_z
-
-    def getTrackerVecX(self):
-        return self.tracker_vec_x
-
-    def getTrackerVecZ(self):
-        return self.tracker_vec_z
-
-    def getADms(self):
-        d, m, s = self.__decimalToDms(self.getA())
-        return f"{d}° {m}' {s}\""
-    
     def getRA(self):
-        h, m, s = self.decimalToHours(self.getA())
-        #we are aligned with the north star, it's position is RA: 2h 31m 48.7s, dec: +89° 15′ 51″
-        # we need to add 2h 31m 48.7s to the current time
-        h += 2
-        m += 31
-        s += 48.7
-        #the same for the declination
-        #h += 89
-        #m += 15
-        #s += 51
+        ra_hours = self.calculateRA()
+        h, m, s = self.decimalToHours(ra_hours * 15)
+        return f"{h}h {m}m {s:.1f}s"
 
-        return f"{h}h {m}m {s}s"
-    
     def getDEC(self):
-        d, m, s = self.__decimalToDms(self.getB())
+        dec_deg = self.calculateDEC()
+        d, m, s = self.__decimalToDms(dec_deg)
         return f"{d}° {m}' {s}\""
 
-    def getBDms(self):
-        d, m, s = self.__decimalToDms(self.getB())
-        return f"{d}° {m}' {s}\""
-
-    def getCDms(self):
-        d, m, s = self.__decimalToDms(self.getC())
-        return f"{d}° {m}' {s}\""
-    
-    def decimalToHours(decimal_degrees):
-        hours = int(decimal_degrees / 15.0)
+    def decimalToHours(self, decimal_degrees):
+        hours = decimal_degrees / 15.0
         h = int(hours)
         minutes = (hours - h) * 60
         m = int(minutes)
@@ -125,4 +80,5 @@ class TrackerController:
         degrees = int(decimal)
         minutes = int((decimal - degrees) * 60)
         seconds = round((((decimal - degrees) * 60) - minutes) * 60, 2)
-        return (degrees, minutes, seconds)
+        return degrees, minutes, seconds
+
